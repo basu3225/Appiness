bdb236b45d36db2b8da0336a616a768af0949ac2
Basu <basavarajk63@gmail.com>
2020-02-01 00:18:59 +0530

    Redux used like store, reducer, action

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,63 +2,9 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import './Dashboard.css';
 
+import { connect } from 'react-redux';
+
 class Dashboard extends Component {
-
-    constructor() {
-        super()
-
-        this.state = {
-            
-                user: [
-                    {
-                        "id":1,
-                        "name":"test1",
-                        "age" : "11",
-                        "gender":"male",
-                        "email" : "test1@gmail.com", "phoneNo" : "9415346313"
-                    },
-                    {
-                        "id" : 2,
-                        "name":"test2",
-                        "age" : "12",
-                        "gender":"male",
-                        "email" : "test2@gmail.com", "phoneNo" : "9415346314"
-                    },
-                    {
-                        "id":3,
-                        "name":"test3",
-                        
-                        "age" : "13",
-                        "gender":"male",
-                        "email" : "test3@gmail.com", "phoneNo" : "9415346315"
-                    },
-                    {
-                        "id":4,
-                        "name":"test4",
-                        "age" : "14",
-                        "gender":"male",
-                        "email" : "test4@gmail.com", "phoneNo" : "9415346316"
-                    },
-                    {
-                        "id":5,
-                        "name":"test5",
-                        "age" : "15",
-                        "gender":"male",
-                        "email" : "test5@gmail.com", "phoneNo" : "9415346317"
-                    },
-                    {
-                        "id":6,
-                        "name":"test6",
-                        "age" : "16",
-                        "gender":"male",
-                        "email" : "test6@gmail.com", "phoneNo" : "9415346318"
-                    }
-                
-                ]
-            }
-                
-        }
-    
 
 render() {
     return(
@@ -81,7 +27,7 @@ render() {
 
                     <tbody>
                          {
-                            this.state.user.map((name,id) => 
+                            this.props.usr.map((name,id) => 
                             <tr key={name.id}>
                                
                                 <td>{name.id}</td>
@@ -101,8 +47,15 @@ render() {
     )
 }
 
-
 }
 
 
-export default Dashboard;
+const mapstatesToProps = state => {
+    return {
+        usr:state.user
+    }
+}
+
+
+
+export default connect(mapstatesToProps) (Dashboard);
